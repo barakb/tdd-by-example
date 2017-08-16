a09ffaa97e3f7eebc41e58c02a2a18aa83ac379f
Barak Bar Orion <barak.bar@gmail.com>
2017-08-16 09:31:42 +0300

    Initial commit

---
+++ src/test/java/org/async/barakb/CacheTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2008-2016, GigaSpaces Technologies, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.async.barakb;
+
+import org.junit.Test;
+
+/**
+ * Created by Barak Bar Orion
+ * on 8/16/17.
+ *
+ * @since 12.1
+ */
+public class CacheTest {
+    @Test
+    public void testAddToDollar() throws Exception{
+
+    }
+}
